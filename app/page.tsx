045df6e70bf5d2c5c39ba095184940c5c9acc493
MarianaES <mariana.enriquez.s@gmail.com>
2025-12-04 23:12:59 -0800

    Include all sections previously created and logic to populate data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 "use client";
 
+import { SetStateAction, useState } from "react";
 import HeroSection from "./components/HeroSection";
+import StatsDisplay from "./components/StatsDisplay";
+import FileUpload from "./components/FileUpload";
+import ActivityHeatmap from "./components/ActivityHeatMap";
+import TopSearches from "./components/TopSearches";
+import WrapUpFoot from "./components/WrapUpFoot";
+
+export interface SearchData {
+  processedData: DailyData[];
+  stats: Stats;
+  topSearches: SearchQuery[];
+}
 
 export interface DailyData {
   date: Date;
@@ -14,6 +26,11 @@ export interface Stats {
   avgPerDay: number;
   maxDay: DailyData;
   minDay: DailyData;
+}
+
+export interface SearchQuery {
+  query: string;
+  count: number;
 }
 
 export interface GoogleTakeOutData {
@@ -30,10 +47,39 @@ export interface GoogleBrowserHistoryData {
 }
 
 export default function Home() {
+  const [stats, setStats] = useState<Stats | null>(null);
+  const [processedData, setProcessedData] = useState<DailyData[] | null>(null);
+  const [topSearches, setTopSearches] = useState<SearchQuery[] | null>(null);
+  console.log("Current stats state:", stats);
+  const handleSuccess = (data: {
+    stats: SetStateAction<Stats | null>;
+    processedData: SetStateAction<DailyData[] | null>;
+    topSearches: SetStateAction<SearchQuery[] | null>;
+  }) => {
+    if (data.stats) {
+      setStats(data.stats);
+    }
+    if (data.processedData) {
+      setProcessedData(data.processedData);
+    }
+    if (data.topSearches) {
+      setTopSearches(data.topSearches);
+    }
+  };
+
+  const handleError = (msg: string) => {
+    console.error(msg);
+  };
   return (
-    <>
+    <div className="min-h-screen flex flex-col items-center justify-center p-10 bg-gradient-to-br from-black via-gray-900 to-black overflow-hidden">
       <HeroSection />
-      {/* <StatsDisplay stats={stats} /> */}
-    </>
+      {!stats && <FileUpload onSuccess={handleSuccess} onError={handleError} />}
+      {processedData && <ActivityHeatmap data={processedData} />}
+      {stats && <StatsDisplay stats={stats} />}
+      {topSearches && <TopSearches searches={topSearches} />}
+      {stats && topSearches && (
+        <WrapUpFoot stats={stats} topSearches={topSearches} />
+      )}
+    </div>
   );
 }
