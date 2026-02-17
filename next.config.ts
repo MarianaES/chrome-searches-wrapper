b68d20574c5dabdda6cbe6bf433ddce15f42acb6
MarianaES <mariana.enriquez.s@gmail.com>
2026-02-16 16:42:57 -0800

    Add base path for repo

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/chrome-searches-wrapper",
   devIndicators: false,
   images: {
     unoptimized: true,
