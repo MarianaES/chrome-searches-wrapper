23741682677f9202588c07e05e9541ccf8b7a4dc
MarianaES <mariana.enriquez.s@gmail.com>
2026-02-18 17:09:01 -0800

    Rever changes to workflow and next config file

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/chrome-searches-wrapper",
   devIndicators: false,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
